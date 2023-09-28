1b243cf283b3b67b1b2183ce6a84e4ffd2e1a142
Samanimkr <Samanimkr@outlook.com>
2023-09-28 17:54:58 +0100

    env vars

--- sageai/utils/format_config_args.py
+++ sageai/utils/format_config_args.py
@@ -1,5 +1,6 @@
 import inspect
 import os
+import json
 
 
 def get_user_package_path():
@@ -13,7 +14,25 @@ def get_user_package_path():
     return os.path.dirname(module_path)
 
 
+def inject_env_vars(**kwargs):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    env_file_path = os.path.join(current_directory, "../env.json")
+
+    if not os.path.exists(env_file_path):
+        return kwargs
+
+    with open(env_file_path) as f:
+        env_vars = json.load(f)
+
+    if env_vars["OPENAI_KEY"]:
+        kwargs["openai_key"] = env_vars["OPENAI_KEY"]
+
+    return kwargs
+
+
 def format_config_args(**kwargs):
+    kwargs = inject_env_vars(**kwargs)
+
     if "functions_directory" not in kwargs:
         kwargs["functions_directory"] = "functions"
     if not os.path.isabs(kwargs["functions_directory"]) and not kwargs[
